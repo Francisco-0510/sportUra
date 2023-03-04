4b84822a4ae32ec43922e78805e9134789e637fa
Francisco-0510 <francisco109623@gmail.com>
2023-03-04 11:30:26 -0600

    Home Page

--- src/app/client/home/home.page.ts
+++ src/app/client/home/home.page.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  cards = [
+    {
+      nombre: 'VicWithGG',
+      fecha: '00/00/00',
+      descripcion:
+        'Describir el objetivo de la tutoría deportiva, cultural o grupal.',
+      imgTuto: 'assets/img/cultural.jpg',
+      imgAutor: 'assets/img/imgAutor.jpg',
+      likes: 15,
+      dislikes: 2,
+      comments: 7,
+      shared: 5,
+    },
+    {
+      nombre: 'VicWithFastGG',
+      fecha: '00/00/00',
+      descripcion:
+        'Describir el objetivo de la tutoría deportiva, cultural o grupal.',
+      imgTuto: 'assets/img/cultural.jpg',
+      imgAutor: 'assets/img/imgAutor.jpg',
+      likes: 15,
+      dislikes: 10,
+      comments: 7,
+      shared: 5,
+    },
+    {
+      nombre: 'VicWithFastGG',
+      fecha: '00/00/00',
+      descripcion:
+        'Describir el objetivo de la tutoría deportiva, cultural o grupal.',
+      imgTuto: 'assets/img/cultural.jpg',
+      imgAutor: 'assets/img/imgAutor.jpg',
+      likes: 15,
+      dislikes: 2,
+      comments: 7,
+      shared: 5,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
